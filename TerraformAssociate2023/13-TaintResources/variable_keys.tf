7fdc56668d3f24577eae61a4b1eaded7df13e88d
Nathan Steckel <nesteck01@aol.com>
2023-02-08 16:02:00 -0500

    removed access keys from file

--- TerraformAssociate2023/13-TaintResources/variable_keys.tf
+++ TerraformAssociate2023/13-TaintResources/variable_keys.tf
@@ -1,7 +1,7 @@
 variable "keys" {
   type = map(any)
   default = {
-    access = "AKIAXGI7XW6EU7WGXRGF",
-    secret = "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"
+    access = "[ACCESS KEY HERE]",
+    secret = "[SECRET KEY HERE]"
   }
 }
