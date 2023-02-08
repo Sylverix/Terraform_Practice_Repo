c744ad73851b842e4571a7c73085506eab5df972
Sylverix <124088369+Sylverix@users.noreply.github.com>
2023-02-08 15:58:15 -0500

    Update variable_defaults.tf

--- TerraformAssociate2023/26-TerraformWorkspace/projects/A/variable_defaults.tf
+++ TerraformAssociate2023/26-TerraformWorkspace/projects/A/variable_defaults.tf
@@ -6,7 +6,7 @@ variable "default-region" {
 variable "keys" {
   type = map(any)
   default = {
-    access = "AKIAXGI7XW6EU7WGXRGF",
-    secret = "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"
+    access = "[ACCESS KEY HERE]",
+    secret = "[SECRET KEY HERE]"
   }
 }
