9869bcbe31dd28a90c91aa19b2b4c90bc198387d
Nathan Steckel <nesteck01@aol.com>
2023-02-09 20:54:35 -0500

    Utilizing DynamoDB to view State Lock data

---
+++ TerraformAssociate2023/28-IntegrateDynamoDBwithS3StateLock/projects/A/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "kplabs-demo-tfstate-bucket-2023"
+    key            = "terraform-dynamodb.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "kplabs-demo-terraform-state-lock-s3"
+  }
+}
